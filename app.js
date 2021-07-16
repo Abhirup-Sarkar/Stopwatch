a1bd374d52321cb2472cc2ef86f893699cb34449
Abhirup Sarkar <76419771+Abhirup-Sarkar@users.noreply.github.com>
2021-07-16 18:33:34 +0530

    added onclick to start button

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+// buttons
+var startButton = document.querySelector("#start")
+var stopButton = document.querySelector("#stop")
+var resetButton = document.querySelector("#Reset")
+
+
+
+function clickButton(){
+    console.log("clicked");
+}
+startButton.addEventListener("click", clickButton)
