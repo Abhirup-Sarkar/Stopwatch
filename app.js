0660c38339e7c8659b832ea6241d7c7a8034c901
Abhirup Sarkar <76419771+Abhirup-Sarkar@users.noreply.github.com>
2021-07-17 13:48:41 +0530

    stopwatch logic

--- app.js
+++ app.js
@@ -1,35 +1,41 @@
+//button
+var startButton = document.querySelector("#start")
+var stopButton = document.querySelector("#stop")
 
 
-// buttons
-// var startButton = document.querySelector("#start")
-// var stopButton = document.querySelector("#stop")
-// var resetButton = document.querySelector("#Reset")
 
-// variable to hold display values
+
+
+//Variables to hold display time values
 
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
 
-//increment seconds, minutes and hours at intervals
-function stopWatch(){
+function clickButtonHandler() {
+    //increment seconds, minutes and hours at intervals
+    
+
     seconds++;
-    if (seconds/60 ===1){
+    if (seconds / 60 === 1) {
         seconds = 0;
         minutes++;
     }
-    if (minutes/60 ===1){
+    if (minutes / 60 === 1) {
         minutes = 0;
         hours++;
     }
+
+        // Display updated display time
+
+    document.getElementById("display").innerHTML = hours + ":" + minutes + ":" + seconds;
+
+    
+
 }
-// Display updated display time
- 
-document.getElementById("display").innerHTML = hours + ":"+ minutes +":"+ seconds;
 
-window.setInterval(stopWatch,1000)
 
-// function clickButton(){
-//     console.log("clicked");
-// }
-// startButton.addEventListener("click", clickButton)
+
+
+
+startButton.addEventListener("start", clickButtonHandler)
