34f84a3f707540fcb3682fdceec738896ef47ba8
Abhirup Sarkar <76419771+Abhirup-Sarkar@users.noreply.github.com>
2021-07-20 18:26:40 +0530

    simplified code and added comments

--- app.js
+++ app.js
@@ -14,24 +14,22 @@ let displayHours = 0;
 let interval = null;
 let status = "stopped"
 
-// function to start or stop 
-
-
+//function for start button
 function startButton() {
     if (status == "stopped") {
-        interval = window.setInterval(stopWatch, 100);
+        interval = window.setInterval(stopWatch, 1000);
         status = "started";
 
     }
 }
-
+//function for stop button
 function stopButton() {
     if (status == "started") {
         clearInterval(interval);
         status = "stopped";
     }
 }
-
+//function for reset button
 function resetButton() {
     if (status == "started" || status == "stopped") {
         window.clearInterval(interval)
@@ -41,33 +39,38 @@ function resetButton() {
         document.getElementById("display").innerHTML = "00:00:00"
         status = "stopped"
     }
-
-
 }
-
+//function for the stopwatch
 function stopWatch() {
 
     seconds++;
     if (seconds / 60 === 1) {
         seconds = 0;
-        minutes++;
-        
+        minutes++;        
     }
-    if (minutes / 60 === 1) {
-        
+    if (minutes / 60 === 1) {        
         minutes = 0;
         hours++;
     }
-    if (seconds < 10 || seconds === 0) {
-        diplaySeconds = "0" + seconds.toString();
-    } else {
+    if (seconds < 10 || seconds == 0) {
+        displaySeconds = "0" + seconds.toString();
+    }
+    else{
         displaySeconds = seconds;
     }
-    if (minutes < 10 || mintues === 0) {
+
+     
+    if (minutes < 10 || mintues == 0) {
         displayMinutes = "0" + minutes.toString();
     }
-    if (hours < 10 || hours === 0) {
+    else{
+        displayMinutes = minutes;
+    }
+    if (hours < 10 || hours == 0) {
         displayHours = "0" + hours.toString();
+    }
+    else{
+        displayHours = hours;
     }
     document.getElementById("display").innerHTML = displayHours + ":" + displayMinutes + ":" + displaySeconds;
 }
