7602bbfe1b7abf72b5d3e464df8e8c9e01942074
Abhirup Sarkar <76419771+Abhirup-Sarkar@users.noreply.github.com>
2021-07-20 16:56:05 +0530

    start, stop, reset working

--- app.js
+++ app.js
@@ -1,53 +1,71 @@
-// var to store start,stop,reset button
-
-var startButton = document.querySelector("#start")
-var stopButton = document.querySelector("#stop")
-
-// var to store leading zeros for display time
-
-let  displaySeconds = 0;
-let displayMinutes = 0;
-let displayHours = 0;
-
-
- 
-
-
-
 //Variables to hold display time values
 
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
 
-function stopWatch(){
+// var to store leading zeros for display time
+
+let displaySeconds = 0;
+let displayMinutes = 0;
+let displayHours = 0;
+//var to hold setInterval
+
+let interval = null;
+let status = "stopped"
+
+// function to start or stop 
+
+
+function startButton() {
+    if (status == "stopped") {
+        interval = window.setInterval(stopWatch, 100);
+        status = "started";
+
+    }
+}
+
+function stopButton() {
+    if (status == "started") {
+        clearInterval(interval);
+        status = "stopped";
+    }
+}
+
+function resetButton() {
+    if (status == "started" || status == "stopped") {
+        window.clearInterval(interval)
+        seconds = 0;
+        minutes = 0;
+        hours = 0;
+        document.getElementById("display").innerHTML = "00:00:00"
+        status = "stopped"
+    }
+
+
+}
+
+function stopWatch() {
+
     seconds++;
-    if(seconds/60 == 1){
+    if (seconds / 60 === 1) {
         minutes++;
         seconds = 0;
     }
-    if(minutes/60 == 1){
+    if (minutes / 60 === 1) {
         hours++;
         minutes = 0;
     }
-    if(seconds<10){
+    if (seconds < 10 || seconds === 0) {
         diplaySeconds = "0" + seconds.toString();
-    }
-    else{
+    } else {
         displaySeconds = seconds;
     }
-    if(minutes<10){
+    if (minutes < 10 || mintues === 0) {
         displayMinutes = "0" + minutes.toString();
     }
-    if(hours<10){
+    if (hours < 10 || hours === 0) {
         displayHours = "0" + hours.toString();
     }
-    display.innerHTML = displayHours +":"+ displayMinutes +":"+ displaySeconds;
+    document.getElementById("display").innerHTML = displayHours + ":" + displayMinutes + ":" + displaySeconds;
 }
-window.setInterval(stopWatch,1000)
-
-
-
-
-
-
